a85a29d2178c976f181c5f66674b88c4aab3350a
jpegjenn <jenn@cacheinteractive.com>
2025-09-08 16:14:24 -0700

    Offset when .in-view is toggled

--- docs/src/scripts/main.js
+++ docs/src/scripts/main.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function($){
     $(window).on('scroll', function () {
         $('.applications-section').each(function () {
             let rect = this.getBoundingClientRect();
-            let inView = (rect.top < window.innerHeight && rect.bottom > 0);
+            let elementMid = rect.top + (rect.height / 2);
+
+            let inView = elementMid < triggerPoint && rect.bottom > 0;
 
             $(this).toggleClass('in-view', inView);
         });
