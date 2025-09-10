b2e21a0041464fe4dfb8a4dc1b57a31ff7038780
jpegjenn <jenn@cacheinteractive.com>
2025-09-10 12:05:39 -0700

    Fix: main nav .fixed trigger

--- docs/src/scripts/main.js
+++ docs/src/scripts/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
     // MAIN NAV MENU
     $(function () {
-        var overview = $('#overview h3');
+        var overview = $('#overview');
         var nav = $('nav#main-menu');
 
         function checkScroll() {
